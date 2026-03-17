07be3764f2ecf84f466934824291a6d2bc983269
BlackFox099 <axiure8@gmail.com>
2026-03-18 00:46:28 +0100

    Inicializacion de modulos basicos

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,9 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
+import { AuthModule } from './auth/auth.module';
+import { UsuariosModule } from './usuarios/usuarios.module';
+import { SwipesModule } from './swipes/swipes.module';
+import { ChatModule } from './chat/chat.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, AuthModule, UsuariosModule, SwipesModule, ChatModule],
   controllers: [AppController],
   providers: [AppService],
 })
